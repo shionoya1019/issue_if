a7daae9d409f27bbae845ae3dd06f644771df71a
Shionoya <shionoya.akari@gmail.com>
2023-12-11 21:00:42 +0900

    fix: if文内の条件の並びを修正

--- src/public/index.php
+++ src/public/index.php
@@ -2,14 +2,16 @@
 echo 'Welcome TECH QUEST!';
 
 //echo "\n";　ブラウザ用の改行ではないので入れても改行されない
+<br><br/>
+
 
 //$num に代入されている値が1~3の場合に[凶]と出力され、4~7の場合に[吉]と出力され、
 //それ以外の場合に[大吉]と出力されるコードを提出してください。
 $num = 8;
 
-if ( $num >= 1 && $num <= 3 ) {
+if (1 <= $num && $num <= 3 ) {
   echo '凶';
-} elseif ( $num >= 4 && $num <= 7 ) {
+} elseif (4 <= $num && $num <= 7 ) {
   echo '吉';
 } else {
   echo '大吉';
